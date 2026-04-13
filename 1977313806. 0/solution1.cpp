a97dfc7e214e7c2265dd126e9a9820d678e8515e
Jinwoo Jung <91457439+wlsdn2749@users.noreply.github.com>
2026-04-13 21:50:59 +0900

    Add solution1.cpp for 1977313806. 0

---
+++ 1977313806. 0/solution1.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        vector<int> result;
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            for(int j=i+1; j<nums.size(); j++)
+            {
