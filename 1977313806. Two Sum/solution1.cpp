0c096c73b105fc6132b6955d551b85458b2b53a9
Jinwoo Jung <91457439+wlsdn2749@users.noreply.github.com>
2026-04-13 21:51:26 +0900

    Add solution1.cpp for 1977313806. Two Sum

---
+++ 1977313806. Two Sum/solution1.cpp
@@ -0,0 +1,10 @@
+                if(target == nums[i] + nums[j])
+                {
+                    result = {i, j};
+                }
+            }
+        }
+
+        return result;
+    }
+};
